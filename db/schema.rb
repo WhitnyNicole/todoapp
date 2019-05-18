52b726ee8de4932279c7ca7a2425da9932e675df
Whitny Edwards <wne4e2@gmail.com>
2019-05-18 02:54:08 +0000

    set up association between todos and users

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190518023651) do
+ActiveRecord::Schema.define(version: 20190518024527) do
 
   create_table "todos", force: :cascade do |t|
-    t.string "name"
-    t.text   "description"
+    t.string  "name"
+    t.text    "description"
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
